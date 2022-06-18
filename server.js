a19acfde52f254d9c6e03ae2217a87e214a47a76
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-06-17 20:03:41 -0700

    get requests working for index and notes html

---
+++ server.js
@@ -0,0 +1,41 @@
+//require dependencies
+
+const express = require ('express');
+const path = require ('path');
+
+//point server to the route files
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//create express server
+const app = express();
+
+//set port
+const PORT = process.env.port || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+//GET route for notes page
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
+
+//setup listener 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
