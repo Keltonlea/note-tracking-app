d69079ff05bfedbc19551d14e51d63909cd62ef1
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-06-21 21:16:34 -0700

    again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,6 @@
 const path = require('path');
 const router = require('express').Router();
 
-// This has to be on the top than the other routers
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
