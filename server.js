3bc4690b5b3e8c5a94afc653a324de6bd00cd5cf
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-06-21 19:31:09 -0700

    added port back

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 
 //set port
-const PORT = process.env.port;
+const PORT = process.env.port || 3001;
+
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
